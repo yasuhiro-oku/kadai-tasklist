0e20a4b69f6faa624d89f1520e9ba3379649bd63
yasuhiro.oku <yshr19990418@gmail.com>
2020-04-13 08:36:49 +0000

    post2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,9 +19,8 @@ class TasksController < ApplicationController
       flash[:success] = 'タスクを投稿しました。'
       redirect_to root_url
     else
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'tasks/index'
+      render 'new'
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
      get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
   
-  resources :tasks, only: [:create, :destroy]
+  
 end
